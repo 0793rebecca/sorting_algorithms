eccbd1ad997ba1647fa73cdf7ab4dbf7265008c6
Worlasi15 <efyawalasi@gmail.com>
2023-08-29 18:17:06 -0700

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,24 +1,40 @@
+#include "sort.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
  * insertion_sort_list - a functon of a sort list
- * @list - sorted list
+ * @list: pointer to the first element of the list
  */
-void insertion_sort_list(listint_t **list);
+void insertion_sort_list(listint_t **list)
 {
-	listint_t *node;
+	listint_t *k, *l;
+	int temp;
 
-	if (list == NULL || (*list)->next == NULL)
+	if (!list || !(*list) || !(*list)->next)
 		return;
-	node = (*list)->next;
-	while(node)
+
+	for (k = (*list)->next; k != NULL; k = k->next)
 	{
-		while ((node->prev) && (node->prev->n > node->n))
+		temp = k->n;
+		l = k->prev;
+
+		while (l != NULL && l->n > temp)
 		{
-			node = swap_node(node, list);
-			print_list(*list)
+			l->next->n = l->n;
+			l = l->prev;
 		}
-		node = node->next;
+
+		if (l == NULL)
+		{
+			(*list)->n = temp;
+		}
+
+		else
+		{
+			l->next->n = temp;
+		}
+
+		print_list(*list);
 	}
 }
