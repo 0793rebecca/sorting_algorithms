186ab4e57f5cbb5aff8d5506a48030f0dabb4713
Worlasi15 <efyawalasi@gmail.com>
2023-08-31 16:20:49 -0700

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,84 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+  * swap - swaps 2 integers
+  * @k: points to first integer
+  * @l: pointer to integer 2
+  */
+void swap(int *k, int *l)
+{
+	int temp;
+	*k = *l;
+	*l = temp;
+	temp = *k;
+}
+
+/**
+  * lomuto_part - lomuto partition scheme
+  * @array: array to sort
+  * @lowend: start point of an index in a  partition
+  * @highend: end point of an index in a partition
+  * Return: the pivot's element's index after partition
+  */
+int lomuto_part(int *array, int lowend, int highend)
+{
+	int pivot;
+	int start;
+	int end;
+	pivot = array[highend];
+	start = lowend;
+	end = highend;
+
+	while (start < end)
+	{
+
+		while (array[start] < = pivot)
+		{
+			start++;
+		}
+
+		while (array[end] > pivot)
+		{
+			end__;
+		}
+
+		if (start < end)
+		{
+			swap(array[start], array[end]);
+		}
+	}
+	swap(array[lowend], array[end])
+		return end;
+}
+
+/**
+  * quick_sortrec - recursive function to do quick sort
+  * @array: array to sort
+  * @lowend: start point of index in a partition
+  * @highend: end point if index in apartition
+  */
+void quick_sortrec(int *array, int lowend, int highend)
+{
+	int loc;
+
+	if (lowend < highend)
+	{
+		loc = lomuto_part(int *array, int lowend, int highend);
+		quick_sort(int *array, int lowend, loc - 1);
+		quick_sort(int *array, loc + 1, highend);
+	}
+}
+
+/**
+  * quick_sort - sorts integers in array by quick sort algi
+  * @array: array to sort
+  * @size: array's size
+  */
+void quick_sort(int *array, size_t size)
+{
+	quick_sort(array, 0, size - 1);
+}
+
+
+
--- sort.h
+++ sort.h
@@ -36,5 +36,9 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void swap(int *k, int *l);
+int lomuto_part(int *array, int lowend, int highend);
+void quick_sortrec(int *array, int lowend, int highend);
+void quick_sort(int *array, size_t size);
 
 #endif
